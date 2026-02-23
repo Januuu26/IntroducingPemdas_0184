9f218a973c3ae4f4c59f15ac25b76c1db8b082c1
Januuu26 <b77286116@gmail.com>
2026-02-23 16:39:00 +0700

    menambahkan variabel age2

--- coba.cpp
+++ coba.cpp
@@ -8,11 +8,13 @@ string nama;
 int main(){ //program utama
     int age;//variabel local
     int age1;//variabel local
+    int age2;
     age = 20;
     age1 = 15;
+    age2 = 10;
     
     cout << "Hello Wold" << endl;
     cout << 99  << endl;
     cout << age << endl;
     cout << age1 << endl;
-}
+    cout << age2 << endl;
