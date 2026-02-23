55461dba0fb8501497a4a39a3f133df20861316d
Januuu26 <b77286116@gmail.com>
2026-02-23 16:43:01 +0700

    menambahkan berat badan

--- coba.cpp
+++ coba.cpp
@@ -9,13 +9,16 @@ int main(){ //program utama
     int age;//variabel local
     int age1;//variabel local
     int age2;
+    int body_weight;
     age = 20;
     age1 = 15;
     age2 = 10;
+    body_weight = 60;
     
     cout << "Hello Wold" << endl;
     cout << 99  << endl;
     cout << age << endl;
     cout << age1 << endl;
     cout << age2 << endl;
+    cout << body_weight << endl;
 }
