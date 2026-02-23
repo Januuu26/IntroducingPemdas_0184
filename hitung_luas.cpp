a8c8e64e93ccbc768cfaf4aa19e1564b67948419
Januuu26 <b77286116@gmail.com>
2026-02-23 16:15:50 +0700

    first commit

---
+++ hitung_luas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p, l, luas;
+
+    //langkah1
+    cout << "Masukan panjang: ";
+    cin >> p;
+
+    //langkah2
+    cout << "Masukan lebar: ";
+    cin >> l;
+
+    //lamgkah3
+    luas = p * l;
+
+    //langkah4
+    cout << "Hasilnya= " << luas;
+
+
+}
