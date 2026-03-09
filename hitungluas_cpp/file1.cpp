c638a38dda5f805fe7b95dc8c883b192b6ef10be
Januuu26 <b77286116@gmail.com>
2026-03-09 15:50:47 +0700

    menambah hasil bilangan yang diberikan

--- hitungluas_cpp/file1.cpp
+++ hitungluas_cpp/file1.cpp
@@ -1,4 +1,4 @@
-#include iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -11,4 +11,6 @@ int main() {
         x = x+1;
         cout<<"Nilai x sudah dikoreksi menjadi "<<x<<endl;
     }
+
+    cout << "Bilangan yang diberikan =" <<x;
 }
